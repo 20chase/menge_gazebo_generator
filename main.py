1addff8eee4d61fa9271a5beff2de729f17a94ee
20chase <tingxiangfan@gmail.com>
2020-01-12 23:01:44 +0800

    correct the menge direcotry

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ os.system("python world_generator.py --scene_name {} --plugin".format(
     scene_name
 ))
 
-os.system("cp -r ./output/{0} ~/menge_ws/Menge/examples/scene".format(scene_name))
+os.system("cp -r ./output/{0} ~/menge_ws/Menge/examples/scene/{0}".format(scene_name))
 os.system("cp ./output/{0}/{0}.world ~/menge_ws/src/menge_gazebo/menge_gazebo_worlds/worlds/{0}.world".format(scene_name))
 
 
