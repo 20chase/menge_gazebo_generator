b3f05d0d328fa1fcf0b62504a6f610a542923e99
20chase <tingxiangfan@gmail.com>
2020-01-14 07:02:45 +0800

    add example and correct the output path

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ cwd = os.getcwd()
 input_dir = cwd + "/example/{}.xml".format(scene_name)
 output_dir = cwd + "/output/" + scene_name 
 
+print("input_dir: ", input_dir)
+print("output_dir: ", output_dir)
+
 os.chdir("MengeFileGenerator")
 os.system("python menge_generator.py {} -o {} -r {}".format(
     input_dir, output_dir, args.resolution
@@ -22,7 +25,7 @@ os.system("python world_generator.py --scene_name {} --plugin".format(
     scene_name
 ))
 
-os.system("cp -r ./output/{0} ~/menge_ws/Menge/examples/scene/{0}".format(scene_name))
+os.system("cp -r ./output/{0} ~/menge_ws/Menge/examples/scene".format(scene_name))
 os.system("cp ./output/{0}/{0}.world ~/menge_ws/src/menge_gazebo/menge_gazebo_worlds/worlds/{0}.world".format(scene_name))
 
 
